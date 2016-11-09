3596d7c5ea0934fe422676665ca4135d6c90803e
Paolo Bueno <paolohaji@gmail.com>
2016-11-09 09:13:54 -0200

    Add load-runner draft script and sample scenario

--- test/client/test-helper.js
+++ test/client/test-helper.js
@@ -2,21 +2,20 @@
 
 var _ = require('lodash');
 var q = require('q');
-var syncTestHelper = {};
 var navigatorOnLine, navigatorOffLine, oldNavigator;
 
-syncTestHelper.startLoggingNotifications = function(stream) {
+exports.startLoggingNotifications = function(stream) {
   var subscription = stream.subscribe(function(event) {
     console.log('\x1b[36m%s\x1b[0m', '** sync event:', event.dataset_id, ':', event.code, ':',  event.message);
   });
   return subscription;
 };
 
-syncTestHelper.stopLoggingNotifications = function(subscription) {
+exports.stopLoggingNotifications = function(subscription) {
   subscription.unsubscribe;
 };
 
-syncTestHelper.syncServerReset = function($fh, datasetId) {
+exports.syncServerReset = function($fh, datasetId) {
   var deferred = q.defer();
   $fh.cloud({
     path: '/sync/reset/' + datasetId,
@@ -29,7 +28,7 @@ syncTestHelper.syncServerReset = function($fh, datasetId) {
   return deferred.promise;
 };
 
-syncTestHelper.syncServerInit = function($fh, datasetId) {
+exports.syncServerInit = function($fh, datasetId) {
   var deferred = q.defer();
   $fh.cloud({
     path: '/sync/init/' + datasetId,
@@ -42,7 +41,7 @@ syncTestHelper.syncServerInit = function($fh, datasetId) {
   return deferred.promise;
 };
 
-syncTestHelper.syncServerStop = function($fh, datasetId) {
+exports.syncServerStop = function($fh, datasetId) {
   var deferred = q.defer();
   $fh.cloud({
     path: '/sync/stop/' + datasetId,
@@ -55,7 +54,7 @@ syncTestHelper.syncServerStop = function($fh, datasetId) {
   return deferred.promise;
 };
 
-syncTestHelper.overrideNavigator = function() {
+exports.overrideNavigator = function() {
   // Overide window.navigator.onLine to make sync work
   if (! oldNavigator) {
     navigatorOnLine = _.clone(navigator);
@@ -67,20 +66,18 @@ syncTestHelper.overrideNavigator = function() {
   }
 };
 
-syncTestHelper.restoreNavigator = function() {
+exports.restoreNavigator = function() {
   if (navigator.oldNavigator) {
     navigator = navigator.oldNavigator;
   }
 };
 
-syncTestHelper.setOnline = function(onLine) {
+exports.setOnline = function(onLine) {
   navigator = onLine ? navigatorOnLine : navigatorOffLine;
 };
 
-syncTestHelper.notificationPromise = function(stream, condition) {
+exports.notificationPromise = function(stream, condition) {
   return stream.filter(function(notification) {
     return _.isMatch(notification, condition);
   }).take(1).toPromise(q.Promise);
 };
-
-module.exports = syncTestHelper;
